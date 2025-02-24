4f61978f1cae191d71796ef88a9d903b8460b624
Hy8012 <henryyost402@gmail.com>
2025-02-23 21:36:22 -0800

    Char counter

---
+++ character/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <map>
+
+int countChar(const std::string& filePath, const std::string& outputFile) {
+    std::ifstream input(filePath);
+    std::ofstream output(outputFile);
+    std::map<char, int> char_freq;
+    
+    if(!input.is_open()) {
+        std::cerr << "Error opening " << filePath << std::endl;
+        input.close();
+        return 1;
+    }
+
+    if(!output.is_open()) {
+        std::cerr << "Error writing to " << outputFile << std::endl;
+        input.close();
+        return 1;
+    }
+
+    char ch;
+    while(input.get(ch)) {
+        char_freq[ch]++;
+    }
+    input.close();
+
+    output << "Character,Count\n";
+    for(const auto& ch_map : char_freq) {
+        output << ch_map.first << "," << ch_map.second << "\n";
+    }
+    output.close();
+    return 0;
+}  
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) { 
+        std::cerr << "Only submit one file" << std::endl;
+        return 1; 
+    }
+    std::string outputFile = "output.txt";
+
+    return countChar(argv[1], outputFile);
+}
